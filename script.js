4a2e699e067327f1a85daac8563a364991b96277
Kylian Deffaut <59844384+kyliandeffaut@users.noreply.github.com>
2025-04-08 09:35:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ document.addEventListener("click", function () {
 
 const apiKey = "09fde116fc4a4080a5829b488fb15ad5";
 
-    // Liste des pays et leurs codes
     const countries = [
       { code: "de", name: "Allemagne", id: "flagDE" },
       { code: "fr", name: "France", id: "flagFR" },
@@ -15,11 +14,9 @@ const apiKey = "09fde116fc4a4080a5829b488fb15ad5";
       { code: "eg", name: "Égypte", id: "flagEG" },
     ];
 
-    // On effectue l'appel API pour les informations, même si elles ne sont pas utilisées ici
     fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${apiKey}`)
       .then(response => response.json())
       .then(data => {
-        // Pour chaque pays, on génère le drapeau
         countries.forEach(country => {
           const flagUrl = `https://flagcdn.com/w320/${country.code}.png`;
           const flagDiv = document.getElementById(country.id);
